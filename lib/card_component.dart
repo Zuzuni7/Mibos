15943d73236c382ed262c5d6c7043f244704ffb7
Shalom Sims <>
2021-11-05 19:52:16 -0400

    reading JSON data now. need to remove button click for load

--- lib/card_component.dart
+++ lib/card_component.dart
@@ -1,67 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-//import 'package:mibos_app/goal_screen.dart';
-//import 'package:mibos_app/models/data.dart';
-//import 'goal_item.dart';
-import 'goals.dart';
+import 'dart:convert';
 
-/** 
- * Card Component should consist of 3 goal_progression components
- * Clicking the Card should open the goals page where goals can be selected
- * 
-*/
-
-class Goals extends StatefulWidget {
-  const Goals({Key? key}) : super(key: key);
-
-  final String goalName = '';
-
-  @override
-  _GoalsState createState() => _GoalsState();
-}
-
-class _GoalsState extends State<Goals> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(30),
-      child: Goal(1, 'Scobity', 'Bop', 'Boppity 2 miles', 'Scoop', 2),
-    );
-  }
-}
-
-// Goal(2, 'Meditate', 'Mind', 'Medidate 20 min', 'min', 20),
-//           Goal(3, 'Pray', 'Spirit', 'Pray 2 times', 'count', 2)
-
-/********************************************************** */
+import 'package:flutter/services.dart';
+import 'goal.dart';
 
 class CardComponent extends StatefulWidget {
-  //const ({ Key? key }) : super(key: key);
+  const CardComponent({Key? key}) : super(key: key);
 
   @override
   _CardState createState() => _CardState();
 }
 
 class _CardState extends State<CardComponent> {
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/data.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["goals"];
+    });
+  }
+
+  Future<void> EditGoals() async {
+    //navigate to Edit goals screen
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 20),
-      alignment: Alignment.center,
+      margin: EdgeInsets.only(top: 25),
+      alignment: Alignment.topCenter,
       height: 275,
       width: 350,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         border: Border.all(color: Colors.black),
       ),
-      child: ListView(
-          padding: EdgeInsets.only(
-              left: 10, top: 25, right: 10, bottom: 10), // space between
-          children: <Widget>[
-            Goal(1, 'Run', 'Body', 'Run 2 miles', 'miles', 2),
-            Goal(1, 'Meditate', 'Mind', 'Meditate 2 min', 'min', 2),
-            Goal(1, 'Pray', 'Spirit', 'Pray 2 times', 'count', 2)
-          ]), // goal progression
+      child: Column(
+        children: [
+          ElevatedButton(
+            child: const Text('Edit Goals'),
+            onPressed: EditGoals,
+          ),
+          // Display the data loaded from sample.json
+          _items.isNotEmpty
+              ? Expanded(
+                  child: ListView.builder(
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        child: ListTile(
+                          leading: Text(_items[index]["id"]),
+                          title: Text(_items[index]["name"]),
+                          subtitle: Text(_items[index]["details"]),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              : Container()
+        ],
+      ),
     );
   }
 }
--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,13 +1,1 @@
-// import 'package:flutter/material.dart';
-// import 'package:mibos_app/goals.dart';
-// import '../goals.dart';
 
-// const GoalList = [
-//   Goal(
-//       id: 1,
-//       name: 'doobee',
-//       type: 'doobee',
-//       details: 'doo',
-//       measurementType: 'bah',
-//       measurementAmount: 626),
-// ];
